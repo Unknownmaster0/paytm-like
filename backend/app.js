45c2f0795893161dea05f0bae416543869073f33
Unknownmaster0 <sagar192002singh@gmail.com>
2024-10-23 23:23:38 +0530

    update the cors policy after deploying the fronted

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,11 @@ const app = express();
 // cors policy
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://localhost:5173',
+      'https://d1ydowo8p03tv9.cloudfront.net',
+    ],
     methods: ['GET', 'POST', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
